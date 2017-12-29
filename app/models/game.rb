afa27a2bee83a0752a814678336518f8d61ea086
Samer Zagzoog <mrsamer.dev@gmail.com>
2017-12-29 21:03:41 +0300

    Added http Link validates

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,6 @@ class Game < ApplicationRecord
   default_url: "/images/:style/missing.png"
   validates_attachment_content_type :thumbnail,
   content_type: /\Aimage\/.*\z/
+
+  validates :link, :format => URI::regexp(%w(http https))
 end
